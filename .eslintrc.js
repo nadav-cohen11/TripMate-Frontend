9de218d68c366b195055fb047c31cd443cf0e38b
Lian Hershkovits <iamlian7575@gmail.com>
2025-04-19 14:53:06 +0300

    initial project setup React, Vite, Tailwind, ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+export default {
+    root: true,
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:prettier/recommended',
+    ],
+    plugins: ['react', 'react-hooks'],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {},
+  };
+  
