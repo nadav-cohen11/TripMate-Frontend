551f353543807f36df3821bb470d062ff06fa23d
Lian Hershkovits <iamlian7575@gmail.com>
2025-06-05 12:50:58 +0300

    fixing the chatList opening when the page is uploading

--- src/pages/Dashboard/Chats/Chat.jsx
+++ src/pages/Dashboard/Chats/Chat.jsx
@@ -12,6 +12,7 @@ const Chats = () => {
   const { user } = useContext(AuthContext);
   const userId = user;
   const socketInstance = useChatSocket(userId, setChats);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   useEffect(() => {
     if (!selectedChatId && chats.length > 0) {
@@ -69,18 +70,37 @@ const Chats = () => {
       >
         TripMate
       </div>
+      <button
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        className='sm:hidden fixed top-4 right-4 z-30 bg-white/80 rounded-full p-2 shadow-md border border-blue-200'
+        aria-label='Toggle chat list'
+      >
+        <svg
+          width='28'
+          height='28'
+          fill='none'
+          stroke='currentColor'
+          strokeWidth='2'
+        >
+          <path d='M4 8h20M4 14h20M4 20h20' />
+        </svg>
+      </button>
       <div className='flex flex-col lg:flex-row min-h-screen pt-24 px-4 lg:px-12 gap-6'>
-        <div className='w-full lg:w-1/3'>
-          <ChatList
-            chats={chats}
-            selectedChatId={selectedChatId}
-            userId={userId}
-            socket={socketInstance}
-            setChats={setChats}
-            setSelectedChatId={setSelectedChatId}
-          />
-        </div>
-        <div className='w-full lg:w-2/3'>
+        {isSidebarOpen && (
+          <div className='w-full lg:w-1/3'>
+            <ChatList
+              chats={chats}
+              selectedChatId={selectedChatId}
+              userId={userId}
+              socket={socketInstance}
+              setChats={setChats}
+              setSelectedChatId={setSelectedChatId}
+              isSidebarOpen={isSidebarOpen}
+              onCloseSidebar={() => setIsSidebarOpen(false)}
+            />
+          </div>
+        )}
+        <div className={`w-full ${isSidebarOpen ? 'lg:w-2/3' : 'lg:w-full'}`}>
           <ChatWindow
             selectedChat={
               selectedChatId
