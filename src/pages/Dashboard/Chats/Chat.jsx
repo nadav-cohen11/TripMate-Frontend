c73758e6f66e75bd56559887daba79208026d5dc
Lian Hershkovits <iamlian7575@gmail.com>
2025-06-19 14:06:19 +0300

    responsive chat

--- src/pages/Dashboard/Chats/Chat.jsx
+++ src/pages/Dashboard/Chats/Chat.jsx
@@ -91,22 +91,20 @@ const Chats = () => {
           <path d='M4 8h20M4 14h20M4 20h20' />
         </svg>
       </button>
-      <div className='flex flex-col lg:flex-row min-h-screen pt-24 px-4 lg:px-12 gap-6'>
-        {isSidebarOpen && (
-          <div className='w-full lg:w-1/3'>
-            <ChatList
-              chats={chats}
-              selectedChatId={selectedChatId}
-              userId={userId}
-              socket={socketInstance}
-              setChats={setChats}
-              setSelectedChatId={setSelectedChatId}
-              isSidebarOpen={isSidebarOpen}
-              onCloseSidebar={() => setIsSidebarOpen(false)}
-            />
-          </div>
-        )}
-        <div className={`w-full ${isSidebarOpen ? 'lg:w-2/3' : 'lg:w-full'}`}>
+      <div className='flex flex-col-reverse sm:flex-row min-h-screen pt-20 sm:pt-24 px-2 sm:px-4 lg:px-12 gap-2 sm:gap-6'>
+        <div className={`w-full sm:w-1/3 max-w-full ${isSidebarOpen ? '' : 'hidden sm:block'}`}>
+          <ChatList
+            chats={chats}
+            selectedChatId={selectedChatId}
+            userId={userId}
+            socket={socketInstance}
+            setChats={setChats}
+            setSelectedChatId={setSelectedChatId}
+            isSidebarOpen={isSidebarOpen}
+            onCloseSidebar={() => setIsSidebarOpen(false)}
+          />
+        </div>
+        <div className={`w-full sm:w-2/3 max-w-full min-w-0`}>
           <ChatWindow
             selectedChat={
               selectedChatId
